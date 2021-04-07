30bba1c3550e8e4a229497fe0c42becf9cdf370d
tejaswini591 <tejt8026>
2021-04-07 17:59:04 +0530

    First boot project

---
+++ DLithe2021Internship/src/main/java/Internship/dlithe/twentyone/DLithe2021Internship/DLithe2021InternshipApplication.java
@@ -0,0 +1,13 @@
+package Internship.dlithe.twentyone.DLithe2021Internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLithe2021InternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DLithe2021InternshipApplication.class, args);
+	}
+
+}
---
+++ DLithe2021Internship/src/test/java/Internship/dlithe/twentyone/DLithe2021Internship/DLithe2021InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package Internship.dlithe.twentyone.DLithe2021Internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLithe2021InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
